62e734cd41fbee1e1da08bbe2baebfbc96b4f298
Ohyung <ohyung@ohyung.com>
2026-07-31 21:25:22 +0900

    refactor: 무작위 RGB 이펙트 소멸 시 HUE 변동 제거 및 고정 무작위 색상 Fade-out으로 개선

--- quantum/rgb_matrix/animations/solid_reactive_multiwide_random_anim.h
+++ quantum/rgb_matrix/animations/solid_reactive_multiwide_random_anim.h
@@ -3,17 +3,35 @@
 RGB_MATRIX_EFFECT(SOLID_REACTIVE_MULTIWIDE_RANDOM)
 #        ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
-static hsv_t SOLID_REACTIVE_MULTIWIDE_RANDOM_math(hsv_t hsv, int16_t dx, int16_t dy, uint8_t dist, uint16_t tick) {
-    uint16_t effect = tick + dist * 5;
-    if (effect > 255) effect = 255;
-    // 파동 위치 및 타임스탬프 기반 무작위 HUE 생성
-    hsv.h = (uint8_t)(g_rgb_timer * 13 + dist * 9 + (dx * 3 ^ dy * 5));
-    hsv.v = qadd8(hsv.v, 255 - effect);
-    return hsv;
-}
-
 bool SOLID_REACTIVE_MULTIWIDE_RANDOM(effect_params_t* params) {
-    return effect_runner_reactive_splash(0, params, &SOLID_REACTIVE_MULTIWIDE_RANDOM_math);
+    RGB_MATRIX_USE_LIMITS(led_min, led_max);
+
+    uint8_t count = g_last_hit_tracker.count;
+    for (uint8_t i = led_min; i < led_max; i++) {
+        RGB_MATRIX_TEST_LED_FLAGS();
+        hsv_t hsv = rgb_matrix_config.hsv;
+        hsv.v     = 0;
+
+        for (uint8_t j = 0; j < count; j++) {
+            int16_t  dx   = g_led_config.point[i].x - g_last_hit_tracker.x[j];
+            int16_t  dy   = g_led_config.point[i].y - g_last_hit_tracker.y[j];
+            uint8_t  dist = sqrt16(dx * dx + dy * dy);
+            uint16_t tick = scale16by8(g_last_hit_tracker.tick[j], qadd8(rgb_matrix_config.speed, 1));
+
+            uint16_t effect = tick + dist * 5;
+            if (effect < 255) {
+                // 타건 지점 및 타임스탬프 기반 고정 무작위 HUE 생성 (Fade 중 색상 변동 없음)
+                uint8_t hit_hue = (uint8_t)(g_last_hit_tracker.x[j] * 13 + g_last_hit_tracker.y[j] * 29 + g_last_hit_tracker.tick[j] * 101 + 37);
+                hsv.h = hit_hue;
+                hsv.v = qadd8(hsv.v, 255 - effect);
+            }
+        }
+
+        hsv.v     = scale8(hsv.v, rgb_matrix_config.hsv.v);
+        rgb_t rgb = rgb_matrix_hsv_to_rgb(hsv);
+        rgb_matrix_region_set_color(params->region, i, rgb.r, rgb.g, rgb.b);
+    }
+    return rgb_matrix_check_finished_leds(led_max);
 }
 
 #        endif // RGB_MATRIX_CUSTOM_EFFECT_IMPLS
--- quantum/rgb_matrix/animations/solid_reactive_simple_random_anim.h
+++ quantum/rgb_matrix/animations/solid_reactive_simple_random_anim.h
@@ -3,16 +3,40 @@
 RGB_MATRIX_EFFECT(SOLID_REACTIVE_SIMPLE_RANDOM)
 #        ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
-static hsv_t SOLID_REACTIVE_SIMPLE_RANDOM_math(hsv_t hsv, uint16_t offset) {
-    if (offset > 255) offset = 255;
-    // 타건 시점 및 오프셋 기반 무작위 HUE 생성
-    hsv.h = (uint8_t)(g_rgb_timer * 11 + offset * 7);
-    hsv.v = scale8(255 - offset, hsv.v);
-    return hsv;
-}
-
 bool SOLID_REACTIVE_SIMPLE_RANDOM(effect_params_t* params) {
-    return effect_runner_reactive(params, &SOLID_REACTIVE_SIMPLE_RANDOM_math);
+    RGB_MATRIX_USE_LIMITS(led_min, led_max);
+
+    uint16_t max_tick = 65535 / qadd8(rgb_matrix_config.speed, 1);
+    for (uint8_t i = led_min; i < led_max; i++) {
+        RGB_MATRIX_TEST_LED_FLAGS();
+        uint16_t tick = max_tick;
+        uint16_t raw_hit_tick = 0;
+
+        // 가장 최근 타건 틱 찾기
+        for (int8_t j = g_last_hit_tracker.count - 1; j >= 0; j--) {
+            if (g_last_hit_tracker.index[j] == i && g_last_hit_tracker.tick[j] < tick) {
+                tick = g_last_hit_tracker.tick[j];
+                raw_hit_tick = g_last_hit_tracker.tick[j];
+                break;
+            }
+        }
+
+        uint16_t offset = scale16by8(tick, qadd8(rgb_matrix_config.speed, 1));
+        if (offset > 255) offset = 255;
+
+        hsv_t hsv = rgb_matrix_config.hsv;
+        if (offset < 255 && tick < max_tick) {
+            // 타건 시점의 고정 틱과 LED 인덱스를 조합한 고정 무작위 HUE (Fade 중 색상 변동 없음)
+            hsv.h = (uint8_t)(raw_hit_tick * 167 + i * 97 + 53);
+            hsv.v = scale8(255 - offset, hsv.v);
+        } else {
+            hsv.v = 0;
+        }
+
+        rgb_t rgb = rgb_matrix_hsv_to_rgb(hsv);
+        rgb_matrix_region_set_color(params->region, i, rgb.r, rgb.g, rgb.b);
+    }
+    return rgb_matrix_check_finished_leds(led_max);
 }
 
 #        endif // RGB_MATRIX_CUSTOM_EFFECT_IMPLS
