bf9e6b8544955a5a03daefd428225fe6b9c8467c
Ohyung <ohyung@ohyung.com>
2026-07-31 22:18:29 +0900

    feat: 무작위 H(색상) & S(채도) 부여 및 V(밝기)만 Fadeout 되도록 이펙트 업데이트

--- quantum/rgb_matrix/animations/solid_reactive_multiwide_random_anim.h
+++ quantum/rgb_matrix/animations/solid_reactive_multiwide_random_anim.h
@@ -6,8 +6,12 @@ RGB_MATRIX_EFFECT(SOLID_REACTIVE_MULTIWIDE_RANDOM)
 static hsv_t SOLID_REACTIVE_MULTIWIDE_RANDOM_math(hsv_t hsv, int16_t dx, int16_t dy, uint8_t dist, uint16_t tick) {
     uint16_t effect = tick + dist * 5;
     if (effect > 255) effect = 255;
-    hsv.h = scale16by8(g_rgb_timer, qadd8(rgb_matrix_config.speed, 8) >> 4);
-    hsv.v = qadd8(hsv.v, 255 - effect);
+
+    // 타건 시점 시드로 무작위 H(색상)와 무작위 S(채도) 선정
+    hsv.h = (uint8_t)(tick * 167 + 53);                // Random H (0~255)
+    hsv.s = (uint8_t)(128 + ((tick * 97 + 31) % 128)); // Random S (128~255 풍부한 채도)
+    hsv.v = qadd8(hsv.v, 255 - effect);                // V(밝기)만 시간이 지나면서 Fadeout
+
     return hsv;
 }
 
--- quantum/rgb_matrix/animations/solid_reactive_simple_random_anim.h
+++ quantum/rgb_matrix/animations/solid_reactive_simple_random_anim.h
@@ -5,8 +5,13 @@ RGB_MATRIX_EFFECT(SOLID_REACTIVE_SIMPLE_RANDOM)
 
 static hsv_t SOLID_REACTIVE_SIMPLE_RANDOM_math(hsv_t hsv, uint16_t offset) {
     if (offset > 255) offset = 255;
-    hsv.h = scale16by8(g_rgb_timer, qadd8(rgb_matrix_config.speed, 8) >> 4);
-    hsv.v = scale8(255 - offset, hsv.v);
+
+    // 타건 시점 시드로 무작위 H(색상)와 무작위 S(채도) 선정
+    uint16_t hit_tick = g_last_hit_tracker.tick[0];
+    hsv.h = (uint8_t)(hit_tick * 167 + 53);                // Random H (0~255)
+    hsv.s = (uint8_t)(128 + ((hit_tick * 97 + 31) % 128)); // Random S (128~255 풍부한 채도)
+    hsv.v = scale8(255 - offset, hsv.v);                   // V(밝기)만 시간이 지나면서 Fadeout
+
     return hsv;
 }
 
